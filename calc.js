7f92ba1dd8b24e121a07b1ea658d466650d6264c
samirrajpurkar <samir.rajpurkar@gmail.com>
2018-11-09 18:36:06 +0400

    got the idea...and done

--- calc.js
+++ calc.js
@@ -1,3 +1,23 @@
 /*
  * Implement all your JavaScript in this file!
  */
+
+accumulator = [ ]
+
+ $('.number').click(function(e) {
+  $('#display').val($('#display').val() + $(this).val());
+ });
+
+ $('#clearButton').click(function(e) {
+  $('#display').val('');
+ });
+
+ $('#addButton').click(function(e) {
+  accumulator.push($('#display').val());
+  console.log(accumulator);
+  $('#display').val('');
+ });
+
+ $('#equalsButton').click(function(e) {
+  $('#display').val(parseInt(accumulator.pop()) + parseInt($('#display').val())); 
+ });
